0e55ea43e130f2c39c555d361a98f45c696aab12
Adriano Araujo <adrianoaraujodsantos@hotmail.com>
2024-07-16 14:40:17 -0300

    chore: fix discord icon component name

--- components/icons/discord-icon.tsx
+++ components/icons/discord-icon.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-export default function GoogleIcon({
+export default function DiscordIcon({
   className,
 }: React.SVGProps<HTMLOrSVGElement>) {
   return (
